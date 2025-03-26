3c81e7672f4924e99bd528455ac4e44f47131113
Johann Medina <hannder_92@hotmail.com>
2025-03-26 17:56:05 -0500

    some components and reducer

---
+++ src/reducers/budget-reducer.ts
@@ -0,0 +1,60 @@
+import { v4 as uuidv4 } from "uuid";
+import { DratfExpense, Expense } from "../types";
+
+export type BudgetActions =
+  | { type: "add-budget"; payload: { budget: number } }
+  | { type: "show-modal" }
+  | { type: "hide-modal" }
+  | { type: "add-expense"; payload: { expense: DratfExpense } };
+
+export type BudgetState = {
+  budget: number;
+  modal: boolean;
+  expenses: Expense[];
+};
+
+export const initialState: BudgetState = {
+  budget: 0,
+  modal: false,
+  expenses: [],
+};
+
+const createExpense = (draftExpense: DratfExpense): Expense => {
+  return {
+    ...draftExpense,
+    id: uuidv4(),
+  };
+};
+
+export const budgetReducer = (
+  state: BudgetState = initialState,
+  action: BudgetActions,
+): BudgetState => {
+  if (action.type === "add-budget") {
+    return {
+      ...state,
+      budget: action.payload.budget,
+    };
+  }
+  if (action.type === "show-modal") {
+    return {
+      ...state,
+      modal: true,
+    };
+  }
+  if (action.type === "hide-modal") {
+    return {
+      ...state,
+      modal: false,
+    };
+  }
+  if (action.type === "add-expense") {
+    const expense = createExpense(action.payload.expense);
+    return {
+      ...state,
+      expenses: [...state.expenses, expense],
+      modal: false,
+    };
+  }
+  return state;
+};
